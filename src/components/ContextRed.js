05fc24191d8cea8e161afe91abfb225cbedcd815
Janvi Goyal <134378919+janvigoyal11@users.noreply.github.com>
2024-04-10 22:50:09 +0530

    Add files via upload

---
+++ src/components/ContextRed.js
@@ -0,0 +1,48 @@
+import React, { createContext, useContext, useReducer } from 'react'
+
+const CartStateContext = createContext();
+const CartDispatchContext = createContext();
+
+const reducer=(state,action)=>{
+    switch(action.type){
+        case "ADD":
+            return [...state,{id: action.id, name: action.name, price: action.price, qty: action.qty, size: action.size, img: action.img}]   // we directly appended in the state
+        
+            case "REMOVE":
+                let newArr=[...state]   // we store state in temporary variable
+                newArr.splice(action.index,1)  // splice means remove, 1 meaning true for removing
+                return newArr;
+            
+            case "UPDATE":
+                let arr=[...state]
+                arr.find((food,index)=>{
+                    if(food.id===action.id){
+                        console.log(food.qty, parseInt(action.qty), action.price + food.price)
+                        arr[index] = { ...food, qty: parseInt(action.qty) + food.qty, price: action.price + food.price }
+                    }
+                    return arr;
+                })
+                return arr;
+
+            case "DROP":
+                let empArray = []
+                return empArray;
+                
+            default:
+                console.log("Error in reducer!");
+    }
+}
+
+export const CartProvider = ({children})=>{
+    const [state,dispatch] = useReducer(reducer,[])
+    return (
+        <CartDispatchContext.Provider value={dispatch}>
+            <CartStateContext.Provider value={state}>
+                {children}
+            </CartStateContext.Provider>
+        </CartDispatchContext.Provider>
+    )
+}
+
+export const useCart = ()=>useContext(CartStateContext);
+export const useDispatch = ()=>useContext(CartDispatchContext);
